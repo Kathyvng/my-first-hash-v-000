33a2cda563791f69530ebe2303aa350a683809bc
Kathy N <kathyvng@gmail.com>
2018-07-09 15:36:44 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,5 +1,5 @@
 def my_hash
-  my_hash = {"my_name" => "Herman Melville"}  
+  my_hash = {"my_name" => "Herman Melville"}
 end
 
 
@@ -27,13 +27,8 @@ def adding
                   "oil paintings" => 3
                  }
   # add 2 muskets to the shipping_manifest hash below
-
-
-
+  shipping_manifest["muskets"] = 2
   # add 4 gun powder to the shipping_manifest hash below
-
-
-
-  # return the shipping_manifest hash below
-
+  shipping_manifest["gun powder"] = 4
+  puts shipping_manifest
 end
